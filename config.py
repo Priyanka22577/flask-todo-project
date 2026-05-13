7c64bacb27785ca405f3818cd977a18dccb030d5
Priyanka22577 <priyankachauhan.959124@gmail.com>
2026-05-13 15:24:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'sangameshbhai')
+    SECRET_KEY = os.getenv('SECRET_KEY', 'priyanka2211')
     AWS_REGION = 'ap-south-1'
     DYNAMODB_TABLE = 'todo_tasks'
     SNS_TOPIC_ARN = os.getenv("SNS_TOPIC_ARN")
-    EMAIL = "sgb080.gitam@gmail.com"
+    EMAIL = "priyankachauhan.959124@gmail.com"
